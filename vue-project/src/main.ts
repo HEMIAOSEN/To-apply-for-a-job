e0be47ae06952e79d9d6acc6423f0e0983f0ab86
hms <1076507557@qq.com>
2024-07-14 17:08:27 +0800

    1.5 配置开发变量

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -2,14 +2,14 @@
  * @Author: hms 1076507557@qq.com
  * @Date: 2024-07-14 16:00:09
  * @LastEditors: hms 1076507557@qq.com
- * @LastEditTime: 2024-07-14 16:40:12
+ * @LastEditTime: 2024-07-14 17:06:19
  * @FilePath: \To-apply-for-a-job\vue-project\src\main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 //引入element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
