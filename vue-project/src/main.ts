48907ffe161be715a4a6ca67119c1d414425bc05
hms <1076507557@qq.com>
2024-07-14 16:05:04 +0800

    初始化

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,4 +1,12 @@
-import './assets/main.css'
+/*
+ * @Author: hms 1076507557@qq.com
+ * @Date: 2024-07-14 16:00:09
+ * @LastEditors: hms 1076507557@qq.com
+ * @LastEditTime: 2024-07-14 16:03:54
+ * @FilePath: \To-apply-for-a-job\vue-project\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
